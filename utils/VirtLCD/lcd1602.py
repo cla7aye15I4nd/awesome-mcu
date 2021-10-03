e1c5b0e173471f670f6aa32a395a959478a7f79c
dataisland <dataisland@outlook.com>
2021-10-03 19:18:36 +0800

    Format the code with class

--- utils/VirtLCD/lcd1602.py
+++ utils/VirtLCD/lcd1602.py
@@ -1,86 +1,94 @@
 import pygame
 import threading
 
-def get_raw_pattern(lo, up):
-    with open('LCD1602A.txt') as f:
-        lines = f.read().splitlines()[lo * 9 + 1: lo * 9 + 9]
-        pattern = [line[up * 6 + 1: up * 6 + 6] for line in lines]
+class LCD:
+    @classmethod
+    def get_raw_pattern(cls, lo, up):
+        with open('LCD1602A.txt') as f:
+            lines = f.read().splitlines()[lo * 9 + 1: lo * 9 + 9]
+            pattern = [line[up * 6 + 1: up * 6 + 6] for line in lines]
 
-    return pattern
+        return pattern
 
-def get_char_pattern(ch):
-    lo = (ord(ch) >> 0) & 0xf
-    up = (ord(ch) >> 4) & 0xf
-    return get_raw_pattern(lo, up)
+    @classmethod
+    def get_char_pattern(cls, ch):
+        lo = (ord(ch) >> 0) & 0xf
+        up = (ord(ch) >> 4) & 0xf
+        return LCD1602.get_raw_pattern(lo, up)
 
-def make_single_line(info):
-    patterns = []
-    for pack in info:
-        if len(pack) == 1:
-            patterns.append(get_char_pattern(pack))
-        else:
-            lo, up = pack
-            patterns.append(get_raw_pattern(lo, up))
+    @classmethod
+    def make_single_line(cls, info):
+        patterns = []
+        for pack in info:
+            if len(pack) == 1:
+                patterns.append(LCD1602.get_char_pattern(pack))
+            else:
+                lo, up = pack
+                patterns.append(LCD1602.get_raw_pattern(lo, up))
 
-    screen = []    
-    for row in range(8):
-        line = ''
-        for pattern in patterns:
-            line += pattern[row]
-        screen.append(line)
-    
-    return screen
-
-def make_screen(infos):
-    screen = []
-    for info in infos:
-        screen += make_single_line(info)
-    return screen
-
-def render(is_activate):
-    FPS = 60
-    runable = True
-
-    size = 10
-    offset = 12
-    
-    row_n, col_n = 8, 5
-    row_c, col_c = 2, 16
-    
-    margin, interval = size * 4, size // 2
-
-    width  = margin * 2 + size * (col_c * col_n) + interval * (col_c - 1)
-    height = margin * 2 + size * (row_c * row_n) + interval * (row_c - 1)
-
-    win = pygame.display.set_mode((width, height))
-
-    clock = pygame.time.Clock()
-
-    pygame.display.set_caption("LCD 1602A")
-
-    while runable:
-        clock.tick(FPS)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                runable = False
+        screen = []    
+        for row in range(8):
+            line = ''
+            for pattern in patterns:
+                line += pattern[row]
+            screen.append(line)
         
-        pygame.draw.rect(win, "#c9f6cd", ((offset, offset), (width - offset*2, height - offset*2)), border_radius=offset)
-        for i in range(row_c * row_n):
-            for j in range(col_c * col_n):
-                x = margin + size * j + interval * (j // col_n)
-                y = margin + size * i + interval * (i // row_n)
+        return screen
 
-                color = "#446644" if is_activate(i, j) else "#bbeebb"
-                pygame.draw.rect(win, color, ((x, y), (size-1, size-1)))
+    @classmethod
+    def make_screen(cls, infos):
+        screen = []
+        for info in infos:
+            screen += LCD1602.make_single_line(info)
+        return screen
 
-        pygame.display.update()
+class LCD1602(LCD):    
+    def __init__(self) -> None:
+        super().__init__()
 
-    print('LCD quit')
-    pygame.quit()
+        self.rows, self.cols = 2, 16
+        self.cheight, self.cwidth = 8, 5        
+        
+        self.data = LCD.make_screen([' ' * self.cols] * self.rows)
+
+    def is_activate(self, i, j):
+        return self.data[i][j] == '#'
+
+    def render(self):
+        size = 10
+        margin, interval = size * 4, size // 2
+
+        width  = margin * 2 + size * (self.cols * self.cwidth) + interval * (self.cols - 1)
+        height = margin * 2 + size * (self.rows * self.cheight) + interval * (self.rows - 1)
+
+        runable = True
+        clock = pygame.time.Clock()
+        win = pygame.display.set_mode((width, height))
+
+        pygame.display.set_caption("LCD 1602A")
+
+        while runable:            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    runable = False
+            
+            clock.tick(60)
+            pygame.draw.rect(win, "#c9f6cd", ((size, size), (width - size*2, height - size*2)), border_radius=size)
+            for i in range(self.rows * self.cheight):
+                for j in range(self.cols * self.cwidth):
+                    x = margin + size * j + interval * (j // self.cwidth)
+                    y = margin + size * i + interval * (i // self.cheight)
+
+                    color = "#446644" if self.is_activate(i, j) else "#bbeebb"
+                    pygame.draw.rect(win, color, ((x, y), (size-1, size-1)))
+
+            pygame.display.update()            
+
+        print('LCD quit')
+        pygame.quit()
 
 if __name__ == '__main__':
-    screen = make_screen(['I guess you are ', 'watching me. ^_^'])
+    screen = LCD1602.make_screen(['I guess you are ', 'watching me. ^_^'])
 
     ## print in term
     print('\n'.join(screen))
@@ -93,13 +101,9 @@ if __name__ == '__main__':
     with open('examples/lcd1602.html', 'w') as f:
         f.write(t.render(info = ''.join(screen)))
 
-    screen = make_screen([' ' * 16] * 2)
-
-    def is_activate(i, j):    
-        return screen[i][j] == '#'
-
-    threading.Thread(target=render, args=(is_activate, )).start()
+    lcd = LCD1602()
+    threading.Thread(target=lcd.render).start()
     
     while True:
         info = input('Screen Info: ').ljust(32, ' ')
-        screen = make_screen([info[0: 16], info[16: 32]])
+        lcd.data = LCD1602.make_screen([info[0: 16], info[16: 32]])

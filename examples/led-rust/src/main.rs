a0f66e47ac928b743eda28f6ff816ed3058a8bd5
dataisland <dataisland@outlook.com>
2021-10-04 22:07:06 +0800

    Rust for STM !!

---
+++ examples/led-rust/src/main.rs
@@ -0,0 +1,40 @@
+//! Demonstrate the use of a blocking `Delay` using the SYST (sysclock) timer.
+
+#![deny(unsafe_code)]
+#![allow(clippy::empty_loop)]
+#![no_main]
+#![no_std]
+
+// Halt on panic
+use panic_halt as _; // panic handler
+
+use cortex_m_rt::entry;
+use stm32f4xx_hal as hal;
+
+use crate::hal::{pac, prelude::*};
+
+#[entry]
+fn main() -> ! {
+    if let (Some(dp), Some(cp)) = (
+        pac::Peripherals::take(),
+        cortex_m::peripheral::Peripherals::take(),
+    ) {
+        let gpioa = dp.GPIOA.split();
+        let mut led = gpioa.pa5.into_push_pull_output();
+        
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+
+        let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
+
+        loop {
+            // On for 1s, off for 1s.
+            led.set_high().unwrap();
+            delay.delay_ms(1000_u32);
+            led.set_low().unwrap();
+            delay.delay_ms(1000_u32);
+        }
+    }
+
+    loop {}
+}

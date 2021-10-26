863c3f641bfce3c63cd0d86a26e0b567529e2377
dataisland <dataisland@outlook.com>
2021-10-26 15:06:49 +0800

    Format and simplify rust code

--- examples/uart-rust/src/main.rs
+++ examples/uart-rust/src/main.rs
@@ -8,21 +8,20 @@
 use panic_halt as _; // panic handler
 
 use core::cell::RefCell;
-use cortex_m::{interrupt::Mutex};
+use cortex_m::interrupt::Mutex;
 use cortex_m_rt::entry;
 use stm32f4xx_hal as hal;
 
-use crate::hal::{pac, prelude::*, stm32, interrupt, serial};
+use crate::hal::{interrupt, pac, prelude::*, serial, stm32};
 use hal::gpio::{gpioa, Alternate};
 
-type STM32F4Serial 
-    = serial::Serial<
-        stm32::USART2,
-        (
-            gpioa::PA2<Alternate<hal::gpio::AF7>>,
-            gpioa::PA3<Alternate<hal::gpio::AF7>>,
-        ),
-    >;
+type STM32F4Serial = serial::Serial<
+    stm32::USART2,
+    (
+        gpioa::PA2<Alternate<hal::gpio::AF7>>,
+        gpioa::PA3<Alternate<hal::gpio::AF7>>,
+    ),
+>;
 
 static HUSART2: Mutex<RefCell<Option<STM32F4Serial>>> = Mutex::new(RefCell::new(None));
 
@@ -34,33 +33,28 @@ fn main() -> ! {
     ) {
         let gpioa = dp.GPIOA.split();
         let mut led = gpioa.pa5.into_push_pull_output();
-        
+
         let rcc = dp.RCC.constrain();
         let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+        let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
 
-        let tx_pin = gpioa.pa2.into_alternate_af7();
-        let rx_pin = gpioa.pa3.into_alternate_af7();
-        
-        dp.USART2.cr1.modify(|_,w|{
-            w.rxneie().set_bit()
-        });
-        
         let mut ser = serial::Serial::new(
             dp.USART2,
-            (tx_pin, rx_pin),
+            (
+                gpioa.pa2.into_alternate_af7(), // tx
+                gpioa.pa3.into_alternate_af7(), // rx
+            ),
             serial::config::Config::default().baudrate(9600.bps()),
             clocks,
         )
         .unwrap();
 
-        ser.listen(serial::Event::Rxne); 
+        ser.listen(serial::Event::Rxne);
         cortex_m::interrupt::free(|cs| *HUSART2.borrow(cs).borrow_mut() = Some(ser));
 
         unsafe {
-            stm32::NVIC::unmask(hal::interrupt::USART2);            
+            stm32::NVIC::unmask(hal::interrupt::USART2);
         }
-
-        let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
 
         loop {
             delay.delay_ms(500_u32);
@@ -76,15 +70,13 @@ fn main() -> ! {
 #[interrupt]
 fn USART2() {
     static mut SER: Option<STM32F4Serial> = None;
-    
+
     let ser = SER.get_or_insert_with(|| {
-        cortex_m::interrupt::free(|cs| {        
-            HUSART2.borrow(cs).replace(None).unwrap()            
-        })
+        cortex_m::interrupt::free(|cs| HUSART2.borrow(cs).replace(None).unwrap())
     });
-        
+
     if ser.is_rxne() {
         let data = ser.read().unwrap();
         ser.write(data).unwrap();
-    }    
+    }
 }
